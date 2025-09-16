b4ef1bca2cdc4328900fe0dd6a440e13b370e56d
Ravi Kishan <kishanravi887321@gmail.com>
2025-09-17 02:42:08 +0530

    Add socket.io server and client implementation for bus tracking functionality

---
+++ server-side/check3.js
@@ -0,0 +1,15 @@
+// user.js
+import { io } from "socket.io-client";
+
+// Connect to server
+const socket = io("ws://localhost:3000");
+
+const busId = "bus_123";
+
+// Tell server this user wants to track a bus
+socket.emit("trackBus", busId);
+
+// Listen for updates
+socket.on("locationUpdate", (data) => {
+  console.log("🚌 Bus update received:", data);
+});
