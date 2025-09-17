303b8cdfbb65c783b63411c9277c9212f74d4664
AntimonyBullets <antimonybullets@gmail.com>
2025-09-17 14:32:59 +0530

    another commit

---
+++ client-side/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          {/* Add more routes here as needed */}
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
